e3622bfa423e9f795d36dc253b1a8362a8551778
devRajeshThapa <devrajeshthapa@gmail.com>
2025-04-29 12:49:38 +0545

    Change ip

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    allowedHosts: ['admin.rajeshthapa69.com.np']
+    allowedHosts: ['admin.grishmainvest.com']
   }
 })
